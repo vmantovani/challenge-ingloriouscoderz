53c9756c8a34129c0dc46252d5e52ed3a8cd0ea9
Vinicius Mantovani <vmantovani@Viniciuss-MBP.home>
2023-09-20 10:04:40 -0300

    Revert "feat: Created the Shape component that can draw both a Rectangle and a Triangle on the screen depending on the type passed via props"
    This reverts commit 58513ebd2bc50f732739236958cf128221244a68.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-// import Rectangle from './Components/Rectangle'
-// import Triangle from './Components/Triangle'
-import { Shape } from './Components/Shape'
+import Rectangle from './Components/Rectangle'
+import Triangle from './Components/Triangle'
 import './Styles/App.css'
 
 const App = () => {
   return (
     <div>
       <div className="container">
-        <Shape type="rectangle" />
-        <Shape type="triangle" />
+        <Rectangle />
+        <Triangle />
       </div>
     </div>
   )
